bdc361f12dbc2d74b85a8275a975beb9aeb9aa80
Crashcrafter <n.hack2810@gmail.com>
2022-06-24 14:20:05 +0200

    removed unused isr, added irq methods

---
+++ src/kernel/i686/irq.c
@@ -0,0 +1,82 @@
+#include "irq.h"
+#include "io.h"
+
+void PIC_sendEOI(unsigned char irq) {
+    if(irq >= 8)
+        i686_outb(PIC2_COMMAND, PIC_EOI);
+
+    i686_outb(PIC1_COMMAND, PIC_EOI);
+}
+
+void PIC_remap() {
+    //Store masks
+    unsigned char a1, a2;
+
+    a1 = i686_inb(PIC1_DATA);
+    a2 = i686_inb(PIC2_DATA);
+
+    //Start init
+    i686_outb(PIC1_COMMAND, ICW1_INIT | ICW1_ICW4);
+    i686_outb(PIC2_COMMAND, ICW1_INIT | ICW1_ICW4);
+
+    //Set offsets since 0-31 are reserved
+    i686_outb(PIC1_DATA, 0x20);
+    i686_outb(PIC2_DATA, 0x28);
+
+    //other init stuff (tell master pic of slave, give slave identity)
+    i686_outb(PIC1_DATA, 4);
+    i686_outb(PIC2_DATA, 2);
+
+    //Set mode
+    i686_outb(PIC1_DATA, ICW4_8086);
+    i686_outb(PIC2_DATA, ICW4_8086);
+
+    //Restore masks
+    i686_outb(PIC1_DATA, a1);
+    i686_outb(PIC2_DATA, a2);
+}
+
+void IRQ_set_mask(unsigned char irqLine) {
+    uint16_t port;
+    uint8_t value;
+
+    if(irqLine < 8) {
+        port = PIC1_DATA;
+    } else {
+        port = PIC2_DATA;
+        irqLine -= 8;
+    }
+
+    value = i686_inb(port) | (1 << irqLine);
+    i686_outb(port, value);
+}
+
+void IRQ_clear_mask(unsigned char irqLine) {
+    uint16_t port;
+    uint8_t value;
+
+    if(irqLine < 8) {
+        port = PIC1_DATA;
+    } else {
+        port = PIC2_DATA;
+        irqLine -= 8;
+    }
+
+    value = i686_inb(port) & ~(1 << irqLine);
+    i686_outb(port, value);
+}
+
+static uint16_t __pic_get_irq_reg(int ocw3) {
+    i686_outb(PIC1_COMMAND, ocw3);
+    i686_outb(PIC2_COMMAND, ocw3);
+
+    return (i686_inb(PIC2_COMMAND) << 8) | i686_inb(PIC1_COMMAND);
+}
+
+uint16_t pic_get_irr() {
+    return __pic_get_irq_reg(PIC_READ_IRR);
+}
+
+uint16_t pic_get_isr() {
+    return __pic_get_irq_reg(PIC_READ_ISR);
+}
--- tools/isr/isr_gen.sh
+++ tools/isr/isr_gen.sh
@@ -14,7 +14,7 @@ ISRS_WITH_ERROR_CODE="8 10 11 12 13 14 17 21 29 30"
 
 # Generate Assembly file
 
-for i in $(seq 0 255); do
+for i in $(seq 0 31); do
   if echo "$ISRS_WITH_ERROR_CODE" | grep -q "\b${i}\b"; then
     echo "ISR_ERRORCODE ${i}" >> $ISRS_GEN_ASM
   else
@@ -24,14 +24,14 @@ done
 
 # Generate C file
 
-for i in $(seq 0 255); do
+for i in $(seq 0 31); do
   echo "void __attribute__((cdecl)) i686_ISR${i}();" >> $ISRS_GEN_C
 done
 
 echo "" >> $ISRS_GEN_C
 echo "void i686_ISR_InitializeGates() {" >> $ISRS_GEN_C
 
-for i in $(seq 0 255); do
+for i in $(seq 0 31); do
   echo "    i686_IDT_SetGate(${i}, i686_ISR${i}, i686_GDT_CODE_SEGMENT, IDT_FLAG_RING0 | IDT_FLAG_GATE_32BIT_INT);" >> $ISRS_GEN_C
 done
 

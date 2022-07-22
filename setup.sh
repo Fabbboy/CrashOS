9db0338bbc68ed4474b39afb37c660b3983f7582
Crashcrafter <n.hack2810@gmail.com>
2022-07-22 16:26:21 +0200

    added missing dependency

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
-apt install build-essential bison flex libgmp3-dev libmpc-dev libmpfr-dev texinfo nasm mtools qemu-system-x86
+apt install build-essential bison flex libgmp3-dev libmpc-dev libmpfr-dev texinfo nasm mtools qemu-system-x86 dosfstools
 make toolchain
 ./run.sh

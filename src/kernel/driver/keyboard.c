f0aee0f427103bc642d9412460b78b5358449111
Crashcrafter <n.hack2810@gmail.com>
2022-07-24 18:02:08 +0200

    implemented alt, implemented #6

--- src/kernel/driver/keyboard.c
+++ src/kernel/driver/keyboard.c
@@ -6,13 +6,13 @@
 unsigned char kbdde_normal[128] =
         {
                 0,  27, '1', '2', '3', '4', '5', '6', '7', '8',	/* 9 */
-                '9', '0', '-', '=', '\b',	/* Backspace */
+                '9', '0', 0, 0, '\b',	/* Backspace */
                 '\t',			/* Tab */
                 'q', 'w', 'e', 'r',	/* 19 */
-                't', 'z', 'u', 'i', 'o', 'p', '[', '+', '\n',	/* Enter key */
+                't', 'z', 'u', 'i', 'o', 'p', 0, '+', '\n',	/* Enter key */
                 0,			/* 29   - Control */
-                'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';',	/* 39 */
-                '\'', '`',   0,		/* Left shift */
+                'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 0,	/* 39 */
+                0, '`',   0,		/* Left shift */
                 '#', 'y', 'x', 'c', 'v', 'b', 'n',			/* 49 */
                 'm', ',', '.', '-',   0,				/* Right shift */
                 '*',
@@ -37,7 +37,7 @@ unsigned char kbdde_normal[128] =
                 0,	/* Page Down */
                 0,	/* Insert Key */
                 0,	/* Delete Key */
-                0,   0,   0,
+                0,   0,   '<',
                 0,	/* F11 Key */
                 0,	/* F12 Key */
                 0,	/* All other keys are undefined */
@@ -45,17 +45,17 @@ unsigned char kbdde_normal[128] =
 
 unsigned char kbdde_shift[128] =
         {
-                0,  27, '!', '"', '#', '$', '%', '&', '/', '(',	/* 9 */
-                ')', '=', '?', '=', '\b',	/* Backspace */
+                0,  27, '!', '"', 0, '$', '%', '&', '/', '(',	/* 9 */
+                ')', '=', '?', 0, '\b',	/* Backspace */
                 '\t',			/* Tab */
                 'Q', 'W', 'E', 'R',	/* 19 */
-                'T', 'Z', 'U', 'I', 'O', 'P', '[', '*', '\n',	/* Enter key */
+                'T', 'Z', 'U', 'I', 'O', 'P', 0, '*', '\n',	/* Enter key */
                 0,			/* 29   - Control */
-                'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ';',	/* 39 */
-                '\'', '`',   0,		/* Left shift */
+                'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 0,	/* 39 */
+                0, '`',   0,		/* Left shift */
                 '\'', 'Y', 'X', 'C', 'V', 'B', 'N',			/* 49 */
                 'M', ';', ':', '_',   0,				/* Right shift */
-                '*',
+                0,
                 0,	/* Alt */
                 ' ',	/* Space bar */
                 0,	/* Caps lock */
@@ -67,17 +67,57 @@ unsigned char kbdde_shift[128] =
                 0,	/* Home key */
                 0,	/* Up Arrow */
                 0,	/* Page Up */
-                '-',
+                0,
                 0,	/* Left Arrow */
                 0,
                 0,	/* Right Arrow */
-                '+',
+                0,
                 0,	/* 79 - End key*/
                 0,	/* Down Arrow */
                 0,	/* Page Down */
                 0,	/* Insert Key */
                 0,	/* Delete Key */
-                0,   0,   0,
+                0,   0,   '>',
+                0,	/* F11 Key */
+                0,	/* F12 Key */
+                0,	/* All other keys are undefined */
+        };
+
+unsigned char kbdde_alt[128] =
+        {
+                0,  27, 0, 0, 0, 0, 0, 0, '{', '[',	/* 9 */
+                ']', '}', '\\', 0, '\b',	/* Backspace */
+                '\t',			/* Tab */
+                0, 0, 0, 0,	/* 19 */
+                0, 0, 0, 0, 0, 0, 0, '~', '\n',	/* Enter key */
+                0,			/* 29   - Control */
+                0, 0, 0, 0, 0, 0, 0, 0, 0, 0,	/* 39 */
+                0, 0,   0,		/* Left shift */
+                0, 0, 0, 0, 0, 0, 0,			/* 49 */
+                0, 0, 0, 0,   0,				/* Right shift */
+                0,
+                0,	/* Alt */
+                ' ',	/* Space bar */
+                0,	/* Caps lock */
+                0,	/* 59 - F1 key ... > */
+                0,   0,   0,   0,   0,   0,   0,   0,
+                0,	/* < ... F10 */
+                0,	/* 69 - Num lock*/
+                0,	/* Scroll Lock */
+                0,	/* Home key */
+                0,	/* Up Arrow */
+                0,	/* Page Up */
+                0,
+                0,	/* Left Arrow */
+                0,
+                0,	/* Right Arrow */
+                0,
+                0,	/* 79 - End key*/
+                0,	/* Down Arrow */
+                0,	/* Page Down */
+                0,	/* Insert Key */
+                0,	/* Delete Key */
+                0,   0,   '|',
                 0,	/* F11 Key */
                 0,	/* F12 Key */
                 0,	/* All other keys are undefined */
@@ -115,11 +155,13 @@ void keyboard_handler(Registers* regs) {
 
         unsigned char c = kbdde_normal[scancode];
 
-        if(c == 0) return;
-
         if(is_key_down(KEY_LSHIFT) || is_key_down(KEY_RSHIFT)) {
             c = kbdde_shift[scancode];
+        } else if(is_key_down(KEY_ALT)) {
+            c = kbdde_alt[scancode];
         }
+
+        if(c == 0) return;
 
         putc(c);
     }

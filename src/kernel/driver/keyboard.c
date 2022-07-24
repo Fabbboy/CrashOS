c39dc93507a0108dc44c1ffd0e582abd023fe11d
Crashcrafter <n.hack2810@gmail.com>
2022-07-24 17:47:06 +0200

    added shift table

--- src/kernel/driver/keyboard.c
+++ src/kernel/driver/keyboard.c
@@ -3,18 +3,58 @@
 #include "../video/stdio.h"
 #include "../i686/io.h"
 
-unsigned char kbdde[128] =
+unsigned char kbdde_normal[128] =
         {
                 0,  27, '1', '2', '3', '4', '5', '6', '7', '8',	/* 9 */
                 '9', '0', '-', '=', '\b',	/* Backspace */
                 '\t',			/* Tab */
                 'q', 'w', 'e', 'r',	/* 19 */
-                't', 'z', 'u', 'i', 'o', 'p', '[', ']', '\n',	/* Enter key */
+                't', 'z', 'u', 'i', 'o', 'p', '[', '+', '\n',	/* Enter key */
                 0,			/* 29   - Control */
                 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';',	/* 39 */
                 '\'', '`',   0,		/* Left shift */
-                '\\', 'y', 'x', 'c', 'v', 'b', 'n',			/* 49 */
-                'm', ',', '.', '/',   0,				/* Right shift */
+                '#', 'y', 'x', 'c', 'v', 'b', 'n',			/* 49 */
+                'm', ',', '.', '-',   0,				/* Right shift */
+                '*',
+                0,	/* Alt */
+                ' ',	/* Space bar */
+                0,	/* Caps lock */
+                0,	/* 59 - F1 key ... > */
+                0,   0,   0,   0,   0,   0,   0,   0,
+                0,	/* < ... F10 */
+                0,	/* 69 - Num lock*/
+                0,	/* Scroll Lock */
+                0,	/* Home key */
+                0,	/* Up Arrow */
+                0,	/* Page Up */
+                '-',
+                0,	/* Left Arrow */
+                0,
+                0,	/* Right Arrow */
+                '+',
+                0,	/* 79 - End key*/
+                0,	/* Down Arrow */
+                0,	/* Page Down */
+                0,	/* Insert Key */
+                0,	/* Delete Key */
+                0,   0,   0,
+                0,	/* F11 Key */
+                0,	/* F12 Key */
+                0,	/* All other keys are undefined */
+        };
+
+unsigned char kbdde_shift[128] =
+        {
+                0,  27, '!', '"', '#', '$', '%', '&', '/', '(',	/* 9 */
+                ')', '=', '?', '=', '\b',	/* Backspace */
+                '\t',			/* Tab */
+                'Q', 'W', 'E', 'R',	/* 19 */
+                'T', 'Z', 'U', 'I', 'O', 'P', '[', '*', '\n',	/* Enter key */
+                0,			/* 29   - Control */
+                'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ';',	/* 39 */
+                '\'', '`',   0,		/* Left shift */
+                '\'', 'Y', 'X', 'C', 'V', 'B', 'N',			/* 49 */
+                'M', ';', ':', '_',   0,				/* Right shift */
                 '*',
                 0,	/* Alt */
                 ' ',	/* Space bar */
@@ -59,16 +99,13 @@ void keyboard_handler(Registers* regs) {
             case 0x4B:
                 arrow_left();
                 return;
-                break;
                 //arrow right
             case 0x4D:
                 arrow_right();
                 return;
-                break;
                 //shift
             case 0x2A:
                 return;
-                break;
         }
 
         if(scancode == 14) {
@@ -76,16 +113,17 @@ void keyboard_handler(Registers* regs) {
             return;
         }
 
-        unsigned char c = kbdde[scancode];
+        unsigned char c = kbdde_normal[scancode];
 
         if(c == 0) return;
 
         if(is_key_down(KEY_LSHIFT) || is_key_down(KEY_RSHIFT)) {
-            if(c <= 'z' && c >= 'a') {
+            /*if(c <= 'z' && c >= 'a') {
                 c -= 'a' - 'A';
             } else if(c <= '9' && c >= '0') {
                 c -= '1' - '!';
-            }
+            }*/
+            c = kbdde_shift[scancode];
         }
 
         putc(c);

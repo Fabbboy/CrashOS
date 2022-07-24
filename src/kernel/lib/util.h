2279a57cce016f6abc9288770876eb76199254ed
Crashcrafter <n.hack2810@gmail.com>
2022-07-24 16:24:02 +0200

    added math functions

--- src/kernel/lib/util.h
+++ src/kernel/lib/util.h
@@ -5,7 +5,3 @@
 //Bit operations
 #define low_16(address) (uint16_t)((address & 0xFFFF))
 #define high_16(address) (uint16_t)(((address >> 16) & 0xFFFF))
-
-//Min Max
-#define fmin(a,b) ((a) < (b) ? (a) : (b))
-#define fmax(a,b) ((a) > (b) ? (a) : (b))
